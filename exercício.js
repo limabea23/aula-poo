e136a814129801226d64901ac42feeafbd51e7d5
Beatriz Lima <beatrizlima.tds1.senai@gmail.com>
2024-08-15 10:31:52 -0300

    Refiz da maneira correta e corrigida pelo professor

--- exercício.js
+++ exercício.js
@@ -1,41 +1,46 @@
-const livros = {
-    título: "Amor & Azeitonas",
-    autor: "Jenna Evans Welch",
-    ano: 2021,
-}
+const livros = new Object ();
+
+livros.título = "Amor & Azeitonas";
+livros.autor = "Jenna Evans Welch";
+livros.ano = 2021;
+
 console.log(livros);
 
-const filme = {
-    título: "A Cinco Passos de Você",
-    diretor: "Justin Baldoni",
-    anoLançamento: 2019,
-}
+const filme = new Object (); 
+
+filme.título = "A Cinco Passos de Você";
+filme.diretor = "Justin Baldoni";
+filme.anoLançamento = 2019;
+
 console.log(filme);
 
-const aluno = {
-    nome: "Beatriz",
-    turma: "2TDS1",
-    matériaFavorita: "Banco de Dados",
-}
+const aluno = new Object ();
+aluno.nome = "Beatriz";
+aluno.turma = "2TDS1";
+aluno.matériaFavorita = "Banco de Dados";
+
 console.log(aluno);
 
-const gato = {
-    nome: "Luna",
-    raça: "vira-lata",
-    cor: "branca e cinza",
-}
+const gato = new Object ();
+
+gato.nome = "Luna";
+gato.raça = "vira-lata";
+gato.cor = "branca e cinza";
+
 console.log(gato);
 
-const carro = {
-    cor: "Rosa",
-    modelo: "Fiat 500",
-    ano: 2012,
-}
+const carro = new Object ();
+
+carro.cor = "Rosa";
+carro.modelo = "Fiat 500";
+carro.ano = 2012;
+
 console.log(carro);
 
-const produto = {
-    descrição: "S20FE Rosa",
-    marca: "Samsung",
-    preço: 2.220,
-}
+const produto = new Object ();
+
+produto.descrição = "S20FE Rosa";
+produto.marca = "Samsung";
+produto.preço = 2.220;
+
 console.log(produto);
