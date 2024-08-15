291208b160a2eb294887e723e5bcb0cb2595672f
Beatriz Lima <beatrizlima.tds1.senai@gmail.com>
2024-08-15 11:38:40 -0300

    Os dois users se apresentam

--- MÉTODO/exercicio.js
+++ MÉTODO/exercicio.js
@@ -4,7 +4,7 @@ class Pessoa {
         this.idade = idade 
         this.altura = altura
     }
-    seApresentar(){
+    seApresentar(){ //método
         console.log(`Oii, eu sou a ${this.nome}, tenho ${this.idade} anos e minha altura é de ${this.altura}.`);
     }
 }
@@ -12,7 +12,8 @@ class Pessoa {
 const user = new Pessoa ("Beatriz", 17, 1.77)
 const user1 = new Pessoa ("Harry Styles", 30, 1.83)
 
-user1.seApresentar()
+user.seApresentar()
+user1.seApresentar() //isso que é método
 
 //console.log(user);
 //console.log(user1);
